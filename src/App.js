216903909d011c6b92f4c4fe5bf7a18100904abf
@reva.y <yana.reva@nixsolutions.com>
2023-10-17 13:37:12 +0300

    create project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Click on the button to open the modal</h1>
+      <button className='open-btn'>Open</button>
+    </div>
+  );
+}
+
+export default App;
